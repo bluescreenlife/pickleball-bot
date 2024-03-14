b6c4bbdc7ac7c3c827a67741763ea2acad5f97b9
bluescreenlife <bluescreenlife91@gmail.com>
2024-03-14 09:33:40 -0500

    fix commenting error

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def webdriver_init():
     service = Service(ChromeDriverManager().install())
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--no-sandbox")
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("window-size=1200x1200")
     chrome_options.add_experimental_option("detach", True)
