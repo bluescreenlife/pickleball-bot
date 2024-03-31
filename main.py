3b7a0a938171655a8e62fb568794d8872d688b78
bluescreenlife <bluescreenlife91@gmail.com>
2024-03-31 13:39:29 -0500

    add error handling, print confrimations, wait time

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, ElementNotInteractableException, ElementClickInterceptedException
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 from datetime import datetime, timedelta
@@ -73,78 +73,109 @@ def register(class_name, date, start_time):
     schedule_url = f"https://my.lifetime.life/clubs/mn/bloomington-north/classes.html?selectedDate={date}&mode=day&location=Bloomington+North"
     driver.get(schedule_url)
 
+    # confirmation/continue variables
+    registration_page = False
     slot_reserved = False
 
-    # start timing
-    pageload_start_time = time.time()
+    # time.sleep(10) # may be necessary when running hosted
 
-    time.sleep(10)
-    
-    # wait for start time elements, select all
-    start_time_elements_rule = (By.XPATH, '//time[@class="time-start"]')
-    start_time_elements = WebDriverWait(driver,30).until(EC.presence_of_all_elements_located(start_time_elements_rule))
+    try:
+        # start timing
+        pageload_start_time = time.time()
+        
+        # find all start time elements
+        start_time_elements_rule = (By.XPATH, '//time[@class="time-start"]')
+        start_time_elements = WebDriverWait(driver,30).until(EC.presence_of_all_elements_located(start_time_elements_rule))
 
-    # end timing, report element load time
-    pageload_end_time = time.time()
-    pageload_total_time = pageload_end_time - pageload_start_time
-    print(f"Located {len(start_time_elements)} court times in {pageload_total_time} seconds.")
+        # end timing, report element load time
+        pageload_end_time = time.time()
+        pageload_total_time = pageload_end_time - pageload_start_time
+        print(f"Located {len(start_time_elements)} court times in {pageload_total_time} seconds.")
+    except (NoSuchElementException, TimeoutException):
+        print("ERROR: Could not locate time elements.")
+        return False
 
+    # iterate through court times, attempt to find match
     for element in start_time_elements:
-        print(f"Checking for time match: {element.text.strip()}")
-        if element.text.strip() == start_time:
+
+        # clean up text, remove any presence of AM or PM from court time text before checking for match
+        # some times on the site include AM/PM unnecessarily
+        element_text = element.text.strip()
+
+        if "AM" in element_text:
+            element_text = element_text.replace("AM", "").strip()
+        if "PM" in element_text:
+            element_text = element_text.replace("PM", "").strip()
+
+        # check for time match
+        print(f"Checking for time match: {element_text}")
+
+        if element_text == start_time:
             target_time_element = element
             print(f"Located target court time: {start_time}")
 
             # get the time element's grandparent element, broader div containing url
-            target_slot = driver.execute_script("return arguments[0].parentNode.parentNode.parentNode;", target_time_element)
-            break
+            container = driver.execute_script("return arguments[0].parentNode.parentNode.parentNode;", target_time_element)
+
+            # get court time registration link and name
+            slot_link = container.find_element(By.XPATH, f".//a[@data-testid='classLink']")
+            print("Located registration link.")
+            slot_title = slot_link.find_element(By.TAG_NAME, "span").text.strip().upper()
+            print(f"Located title: {slot_title}")
+
+            # check slot title against desired class name
+            if slot_title == class_name.upper():
+                print("Court time and name match. Continuing...")
+
+                # click link for target time slot
+                slot_link.click()
+                print("Clicked target court time link...")
+
+                registration_page = True
+                break
+            else:
+                print(f"Title of court time does not match target {class_name}\nLink not clicked.")
+        else:
+            pass
 
     # if time slot found, continue with registration, else return error message
-    if target_slot:
-        # get slot name
-        slot_link = target_slot.find_element(By.XPATH, f".//a[@data-testid='classLink']")
-        slot_title = slot_link.find_element(By.TAG_NAME, "span").text.strip().lower()
-        print(f"Slot title: {slot_title}")
-
-        # check slot title against desired class name
-        if slot_title == class_name.lower():
-            print("Court time name match. Continuing...")
-
-            # click link for target time slot
-            slot_link.click()
-            print("Clicked target court time link...")
-
-            # wait for reserve button to be present, click reserve button
-            try:
-                # start timing
-                pageload_start_time = time.time()
-
-                reserve_button_rule = (By.XPATH, ".//button[@data-testid='reserveButton']")
-                reserve_buttons = WebDriverWait(driver,15).until(EC.presence_of_all_elements_located(reserve_button_rule)) # seemingly 2 of these elements, one hidden
-                
-                # end timing, report element load time
-                pageload_end_time = time.time()
-                pageload_total_time = pageload_end_time - pageload_start_time
-                print(f"Reserve button located in {pageload_total_time} seconds.")
-
-                # click reserve button
-                reserve_buttons[1].click()
-                print("Clicked reserve button...")
-            except NoSuchElementException:
-                print("ERROR: Reserve button not available.")
-                return False
-            except IndexError:
-                print("ERROR: Reserve button not available.")
-                return False
-            except TimeoutException:
-                print("ERROR: Reserve button load timeout.")
-                return False
-
-            # wait for agreement box presence, click agreement box
-
+    if registration_page:
+        # locate reserve buttons
+        try:
             # start timing
             pageload_start_time = time.time()
 
+            reserve_button_rule = (By.XPATH, ".//button[@data-testid='reserveButton']")
+            # 2 reserve button elements, active one depends on window size
+            reserve_buttons = WebDriverWait(driver,15).until(EC.presence_of_all_elements_located(reserve_button_rule))
+            
+            # end timing, report element load time
+            pageload_end_time = time.time()
+            pageload_total_time = pageload_end_time - pageload_start_time
+            print(f"Reserve buttons located in {pageload_total_time} seconds.")
+        except (NoSuchElementException, TimeoutException):
+            print("ERROR: Reserve button(s) not located.")
+            return False
+
+        # click reserve button
+        try:
+            reserve_buttons[1].click()
+            print("Clicked second (large window size) reserve button...")
+        except (ElementNotInteractableException, ElementClickInterceptedException):
+            print("Second button not clickable. Trying first button.")
+            try:
+                reserve_buttons[0].click()
+                print("Clicked first (small window size) reserve button.")
+            except (ElementNotInteractableException, ElementClickInterceptedException):
+                print("First button not clickable.\nERROR: No reserve buttons were clickable.")
+                return False
+
+        # wait for agreement box presence, click agreement box
+        try: 
+            # start timing
+            pageload_start_time = time.time()
+
+            # locate agreement box
             agreement_box_rule = (By.XPATH, ".//span[@class='c-indicator']")
             agreement_box = WebDriverWait(driver,15).until(EC.element_to_be_clickable(agreement_box_rule))
 
@@ -155,25 +186,39 @@ def register(class_name, date, start_time):
 
             agreement_box.click()
             print("Clicked consent box...")
+        except (NoSuchElementException, TimeoutException, ElementClickInterceptedException, ElementNotInteractableException):
+            print("ERROR: Could not locate/click consent box.")
+            return False
 
-            # wait for finish button to be clickable, then click
+        # wait for finish button to be clickable, then click
+        try:
             finish_button_rule = (By.XPATH, ".//button[@data-testid='finishBtn']")
             finish_button = WebDriverWait(driver,15).until(EC.element_to_be_clickable(finish_button_rule))
             finish_button.click()
             print("Clicked finish button. Waiting for confirmation page...")
+        except (NoSuchElementException, TimeoutException, ElementClickInterceptedException, ElementNotInteractableException):
+            print("ERROR: Could not locate/click finish button.")
+            return False
 
+        # wait for confirmation header to be found, then confirm registration
+        try:
+            # start timing
+            pageload_start_time = time.time()
+
+            # locate confirmation header
             confirmation_header_rule = (By.XPATH, ".//h1[@data-testid='confirmationHeader']")
             confirmation_header = WebDriverWait(driver,30).until(EC.presence_of_element_located(confirmation_header_rule))
 
+            # end timing, report element load time
+            pageload_end_time = time.time()
+            pageload_total_time = pageload_end_time - pageload_start_time
+            print(f"Confirmation header located in {pageload_total_time} seconds.")
+
             if confirmation_header:
-                print("Located confirmation header.")
                 slot_reserved = True
-        else:
-            print(f"ERROR: class name and start time mismatch.")
+        except (NoSuchElementException, TimeoutException):
+            print("ERROR: Confirmation header not located.")
             return False
-    else:
-        print("Could not locate specified time slot.")
-        return False
 
     # confirm reservation and return status
     if slot_reserved:
@@ -189,7 +234,6 @@ def register(class_name, date, start_time):
     # Monday @ 11AM for next week's Tuesday @ 9AM
     # Wednesday @ 11AM for next week's Thursday @ 9AM
     # Thursday @ 3PM for next week's Friday @ 1PM
-
 
 if __name__ == "__main__":
     # wait to ensure registration is live
@@ -222,6 +266,12 @@ if __name__ == "__main__":
         print(f"Weekday is currently {weekday}; not a designated registration time.")
         class_start = "NONE"
         class_name = "NONE"
+
+    # test code
+    print("Running TEST:")
+    class_name = "Pickleball Open Play - ARORA"
+    class_date = "2024-04-08" # format 2024-03-31
+    class_start = "11:00"
 
     # print confirmation
     print(f"Timestamp: {weekday}, {date}")
